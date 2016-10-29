c1873dbd226666fbf3b61ed8341b2906e1ad6989
suyama <hlikekt@yahoo.co.jp>
2016-10-29 21:52:09 +0900

    ブランチコミットテスト

---
+++ sampleProject01/src/sample/test/sym/Test02.java
@@ -0,0 +1,14 @@
+package sample.test.sym;
+
+public class Test02 {
+	public static void main(String[] args) {
+
+		// ƒNƒ‰ƒX•Ï”‚ğ’è‹`
+		int i = 0;
+		int k = 10;
+
+		int j = i + k;
+
+		System.out.println(j);
+	}
+}
